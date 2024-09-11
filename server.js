9fbe59b8c9a2268da9a5a27084529f7c43fa32c4
DSSID-DEV <128099997+DSSID-DEV@users.noreply.github.com>
2024-09-10 23:22:17 -0300

    Update server.js
    Signed-off-by: DSSID-DEV <128099997+DSSID-DEV@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('./src/app')
 const { baseWebhookURL } = require('./src/config')
-const https = require('https')
 require('dotenv').config()
 
 // Start the server
@@ -12,9 +11,6 @@ if (!baseWebhookURL) {
   process.exit(1) // Terminate the application with an error code
 }
 
-https.createServer(app.options, app).listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
-//app.listen(port, () => {
-//  console.log(`Server running on port ${port}`)
-//})
