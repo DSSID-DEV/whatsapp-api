b3691fe188bbf7be84d0f6662d4265319b9b7f1a
DSSID-DEV <128099997+DSSID-DEV@users.noreply.github.com>
2024-09-28 20:13:31 -0300

    Refatora server e .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const https = require('https')
-const fs = require('fs');
+// const https = require('https')
+// const fs = require('fs');
 const app = require('./src/app')
 
 const { baseWebhookURL } = require('./src/config')
@@ -8,10 +8,10 @@ require('dotenv').config()
 // Start the server
 const port = process.env.PORT || 3000
 
-const options = {
-  key: fs.readFileSync('/root/key.pem'),
-  cert: fs.readFileSync('/root/cert.pem'),
-};
+// const options = {
+//   key: fs.readFileSync('/root/key.pem'),
+//   cert: fs.readFileSync('/root/cert.pem'),
+// };
 
 // Check if BASE_WEBHOOK_URL environment variable is available
 if (!baseWebhookURL) {
@@ -20,10 +20,10 @@ if (!baseWebhookURL) {
 }
 
 
-https.createServer(options, app).listen(port, () => {
-  console.log('Servidor HTTPS rodando em https://<SEU_IP>:443');
-});
+// https.createServer(options, app).listen(port, () => {
+//   console.log('Servidor HTTPS rodando em https://<SEU_IP>:443');
+// });
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
