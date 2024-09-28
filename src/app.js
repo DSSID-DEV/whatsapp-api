c834735b06983a4ed88fe03d390a3da3225f8b73
DSSID-DEV <128099997+DSSID-DEV@users.noreply.github.com>
2024-09-28 09:04:01 -0300

    volta ao original

--- src/app.js
+++ src/app.js
@@ -8,24 +8,25 @@ const bodyParser = require('body-parser')
 const { maxAttachmentSize } = require('./config')
 
 
-const allowedOrigins = ['https://feature-dev--e-post.netlify.app/', 'https://disparo-de-message-api-a27cfb7ca502.herokuapp.com'];
+// const allowedOrigins = ['https://feature-dev--e-post.netlify.app/', 'https://disparo-de-message-api-a27cfb7ca502.herokuapp.com'];
 
-// Initialize Express app
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
-    // credentials: true // Se você precisar enviar cookies ou cabeçalhos de autenticação
-  };
+// // Initialize Express app
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     // allowedHeaders: ['Content-Type', 'Authorization'],
+//     // credentials: true // Se você precisar enviar cookies ou cabeçalhos de autenticação
+//   };
 
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions))
+app.use(cors())
+// app.use(cors(corsOptions))
+// app.options('*', cors(corsOptions))
 app.disable('x-powered-by')
 
 app.use((req, res, next) => {
