b0116294034bfd481691a9ed07aad4a71b9c0350
DSSID-DEV <128099997+DSSID-DEV@users.noreply.github.com>
2024-09-28 07:23:51 -0300

    reajuste do cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.disable('x-powered-by')
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://feature-dev--e-post.netlify.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     if (req.method === 'OPTIONS') {
